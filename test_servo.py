1590f53239fc305a4ebd12999ae9566a7eaf0b0c
Chris Albertson <albertson.chris@gmail.com>
2022-11-25 00:11:28 -0800

    test.servo.py works

--- test_servo.py
+++ test_servo.py
@@ -1,16 +1,37 @@
+"""Demo program to show and test the server driver"""
+
 import time
+import numpy as np
+import math
 import servo
 
 s = servo.Servo()
 
 
 def main():
-    s.move_radian(0, 3.14159/2.0)
-    """while True:
-        time.sleep(1.0)
-        s.set_degrees(0, 70.0)
-        time.sleep(0.5)
-        s.set_degrees(0, 110.0)"""
+
+    midpoint = 90.0
+    peak = 10.0
+    angles = np.array([midpoint for i in range(16)])
+
+    movement_period = 2.0
+    loop_period = 1.0/20.0
+
+    # To prevent a large current draw on the power supply, move the motors
+    # one at a time to the neutral position.
+    for ch in range(16):
+        s.move_radian(ch, midpoint)
+        time.sleep(0.25)
+
+    # Move motors slowly around the midpoint
+    start_time = time.time()
+    while True:
+        tick = time.time()
+        phase_angle = 2.0 * math.pi * (((tick - start_time) % movement_period) / movement_period)
+
+        s.move_16_radian(angles + (peak * math.sin(phase_angle)))
+
+        time.sleep(loop_period - (time.time()-tick))
 
 if __name__ == '__main__':
     main()
