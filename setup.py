3b279047de44d24d9623d43bd800a4ba90273254
Chris Albertson <albertson.chris@gmail.com>
2022-12-13 12:30:32 -0800

    added dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup
 setup(name='pca9685_psd',
       version='0.1',
       description='PCA9685 Precision Servo Driver',
-      author='Chris Albertson', url = '#',
+      author='Chris Albertson',
+      url='#',
       license='GNU LGPL',
       author_email='albertson.chris@gmail.com',
       packages=['pca9685_psd'],
       package_data={'': ['*.yaml']},
-      install_requires=['smbus2'],
+      install_requires=[
+          'smbus2',
+          'yaml',
+          'numpy',
+          'matplotlib',
+          'scipy',
+          'PySimpleGUI'
+      ],
       zip_safe=False)
